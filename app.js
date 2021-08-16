8fa77f055332945ea4373be1382dfe73b5b2d0ce
Emily <emilymarchetti@outlook.com>
2021-08-16 17:44:14 -0300

    Criado um tela pra consulta CEP

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const request = require('request');
+const favicon = require('serve-favicon');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/getCep', function (req, res) {
+    var cep = req.query.cep;
+    request('https://viacep.com.br/ws/' + cep + '/json/', function (error, response, json) {
+        res.send(json);
+    });
+});
+
+app.get('/getUf', function (req, res) {
+    request('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome', function (error, response, json) {
+        res.send(json);
+    });
+});
+
+app.get('/getCidades', function (req, res) {
+    var uf = req.query.uf;
+    request('https://servicodados.ibge.gov.br/api/v1/localidades/estados/' + uf + '/municipios?orderBy=nome', function (error, response, json) {
+        res.send(json);
+    });
+});
+
+app.get('/getCepByEndereco', function (req, res) {
+    var uf = req.query.uf.toLowerCase();
+    var localidade = req.query.localidade.toLowerCase();
+    var logradouro = req.query.logradouro.toLowerCase();
+
+    request('https://viacep.com.br/ws/' + uf + '/' + localidade + '/' + logradouro + '/json/', function (error, response, json) {
+        res.send(json);
+    });
+});
+
+app.use('/', router);
+app.use(express.static('public'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.listen(process.env.port || 3000);
---
+++ public/js/index.js
@@ -0,0 +1,198 @@
+function clearFields() {
+    $('#str_logradouro').val('');
+    $('#str_bairro').val('');
+    $('#str_localidade').val('');
+    $('#str_uf').val('');
+    $('#str_ibge').val('');
+    $('#str_ddd').val('');
+}
+
+function searchByEndereco() {
+    var uf = $('#cmb_uf option:selected').text();
+    var localidade = $('#cmb_localidade option:selected').text();
+    var logradouro = $('#str_logradouro_search').val();
+
+    if (!uf) {
+        alert('Por favor selecione uma UF.');
+        return;
+    }
+
+    if (!localidade) {
+        alert('Por favor selecione uma localidade.');
+        return;
+    }
+
+    if (!logradouro) {
+        alert('Por favor informe um logradouro.');
+        return;
+    }
+
+    $.ajax({
+        url: '/getCepByEndereco',
+        data: { 'uf': uf, 'localidade': localidade, 'logradouro': logradouro },
+        type: 'GET',
+        beforeSend: function () {
+            $('#loading').show();
+        },
+        complete: function () {
+            $('#loading').hide();
+        },
+        dataType: 'json',
+        success: buildTableCeps,
+        error: function () {
+            alert('Não foi possível buscar o CEP por endereço.');
+        }
+    });;
+}
+
+function buildTableCeps(response) {
+    if ('erro' in response) {
+        alert('Nenhuma UF encontrada');
+        return;
+    }
+
+    response = response.slice(0, 10); // Mostro apenas os 10 primeiros ceps encontrados
+    var $table_cep_tbody = $('#table_cep tbody');
+    $table_cep_tbody.empty();
+    var itens_html = '';
+
+    $.each(response, function (index, item) {
+        itens_html += '<tr>' +
+            '<td></td>' +
+            '<td>' + item.cep + '</td>' +
+            '<td>' + item.logradouro + '</td>' +
+            '<td>' + item.bairro + '</td>' +
+            '<td>' + item.complemento + '</td>' +
+            '</tr>';
+    });
+    $table_cep_tbody.append(itens_html);
+}
+
+function getUf() {
+    $.ajax({
+        url: '/getUf',
+        type: 'GET',
+        dataType: 'json',
+        success: setUf,
+        error: function () {
+            alert('Não foi possível buscar os estados.');
+        }
+    });
+}
+
+function setUf(response) {
+    if ('erro' in response) {
+        alert('Nenhuma UF encontrada');
+        return;
+    }
+
+    var $cmb_uf = $('#cmb_uf');
+    $cmb_uf.empty();
+
+    $cmb_uf.append($('<option>', {
+        value: '',
+        text: ''
+    }));
+
+    $.each(response, function (index, item) {
+        $cmb_uf.append($('<option>', {
+            value: item.id,
+            text: item.sigla
+        }));
+    });
+}
+
+function getCidades() {
+    var uf = $('#cmb_uf').val();
+    var $cmb_localidade = $('#cmb_localidade');
+
+    if (!uf) {
+        $cmb_localidade.val();
+        $cmb_localidade.attr('disabled', 'disabled');
+        return;
+    }
+
+    $cmb_localidade.removeAttr('disabled');
+
+    $.ajax({
+        url: '/getCidades',
+        data: { 'uf': uf },
+        type: 'GET',
+        beforeSend: function () {
+            $('#loading').show();
+        },
+        complete: function () {
+            $('#loading').hide();
+        },
+        dataType: 'json',
+        success: setCidades,
+        error: function () {
+            alert('Não foi possível buscar as cidades.');
+            clearFields();
+        }
+    });;
+}
+
+function setCidades(response) {
+    if ('erro' in response) {
+        alert('Nenhuma cidade encontrada');
+        return;
+    }
+
+    var $cmb_localidade = $('#cmb_localidade');
+    $cmb_localidade.empty();
+
+    $.each(response, function (index, item) {
+        $cmb_localidade.append($('<option>', {
+            value: item.id,
+            text: item.nome
+        }));
+    });
+}
+
+function buscarCep() {
+    var cep = $('#str_cep').val();
+
+    $.ajax({
+        url: '/getCep',
+        data: { 'cep': cep },
+        type: 'GET',
+        dataType: 'json',
+        success: setCep,
+        error: function () {
+            alert('Não foi possível buscar o CEP.');
+            clearFields();
+        }
+    });;
+}
+
+function setCep(response) {
+    if ('erro' in response) {
+        clearFields();
+        alert('CEP não encontrado.')
+        return;
+    }
+    $('#str_logradouro').val(response.logradouro);
+    $('#str_bairro').val(response.bairro);
+    $('#str_localidade').val(response.localidade);
+    $('#str_uf').val(response.uf);
+    $('#str_ibge').val(response.ibge);
+    $('#str_ddd').val(response.ddd);
+}
+
+
+$(document).ready(function () {
+    var $str_cep = $('#str_cep');
+    var $cmb_uf = $('#cmb_uf');
+    $str_cep.mask('00000-000', { reverse: true });
+
+    $str_cep.off('change').on('change', function () {
+        buscarCep();
+    });
+
+    $cmb_uf.off('change').on('change', function () {
+        getCidades();
+    });
+
+    getUf();
+});
